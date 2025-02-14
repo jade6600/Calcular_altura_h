8879fcdd797e93aa9c77cc531eb3c78f06a4f055
jade6600 <sortehflorez@colegioguanenta.edu.co>
2025-02-14 07:08:22 -0500

    se agrego el programa.py

--- calcular_altura_h.py
+++ calcular_altura_h.py
@@ -1,14 +1,14 @@
 # input
-h=float(input("Digite el valor inicial de (h):"))
+h=int(input("Digite el valor inicial de (h):"))
 
 # processig
 q=h/5
 n=0
 
-while h > q:
+while h >= q:
     n=n+1
     h=0.9*h
 
 
 #output
-print("la pelota no alcanza a subir la quinta parte de la altura inicial en el rebote numero:",rebotes)
+print("la pelota no alcanza a subir la quinta parte de la altura inicial en el rebote numeros:"+str(n))
